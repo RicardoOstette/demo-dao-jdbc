9a57f5d3502e3e08f761a8d5b02680f7af42ee86
Ricardo Ostette <ostettericardo@gmail.com>
2026-03-17 23:22:02 +0100

    findById implementation Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,21 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		IO.println("=== Test 1: Department fyndById ===");
+		Department department = departmentDao.findById(3);
+		IO.println(department);
+		
+		
+		
 	}
 
 }
